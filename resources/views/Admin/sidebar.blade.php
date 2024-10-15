d47aa9c77d7b26cf733a78517185cd8d9c673322
softlancerIn <softlancer.in@gmail.com>
2024-10-15 12:52:04 +0530

    Update sidebar.blade.php
    Signed-off-by: softlancerIn <softlancer.in@gmail.com>

--- resources/views/Admin/sidebar.blade.php
+++ resources/views/Admin/sidebar.blade.php
@@ -46,14 +46,7 @@
                 <div data-i18n="Analytics" class="mx-2">Company</div>
             </a>
         </li>
-        <li class="menu-item {{$active == 'payout' ? 'active': ''}}">
-            <a href="{{route('refundList')}}" class="menu-link">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5" />
-                </svg>
-                <div data-i18n="Analytics" class="mx-2">PayOut</div>
-            </a>
-        </li>
+        
         <li class="menu-item {{$active == 'comission' ? 'active': ''}}">
             <a href="{{route('comission')}}" class="menu-link">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
@@ -99,6 +92,7 @@
                 <div data-i18n="Analytics" class="mx-2">Bank</div>
             </a>
         </li>
+        
         <li class="menu-item">
             <div class="accordion accordion-flush mt-1" id="accordionFlushExample">
                 <div class="accordion-item">
@@ -147,7 +141,14 @@
             </div>
         </li>
 
-
+        <li class="menu-item {{$active == 'payout' ? 'active': ''}}">
+            <a href="{{route('refundList')}}" class="menu-link">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5" />
+                </svg>
+                <div data-i18n="Analytics" class="mx-2">PayOut</div>
+            </a>
+        </li>
 
 
 
