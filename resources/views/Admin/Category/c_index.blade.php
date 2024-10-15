70794ee15527392c7eee4339731f5987ac408d09
softlancerIn <softlancer.in@gmail.com>
2024-10-15 20:32:24 +0530

    Update c_index.blade.php
    Signed-off-by: softlancerIn <softlancer.in@gmail.com>

--- resources/views/Admin/Category/c_index.blade.php
+++ resources/views/Admin/Category/c_index.blade.php
@@ -30,7 +30,7 @@ break;
 @endphp
 <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
   
-<script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+
 <style>
     .cus_btn_padding {
         padding: 0.4345rem 0.5rem;
@@ -309,6 +309,7 @@ break;
         </div>
     </div>
 </div>
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
 <script>
     function change_user_status(status, id, tableType) {
         var check = confirm('Are You sure to want to change this transetion status!');
